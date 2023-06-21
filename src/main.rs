66aaea22d9b8d91c1fc5557df3ac5c3d9f4a0487
Zachary Kohnen <me@dusterthefirst.com>
2023-06-21 14:18:38 +0200

    egui but simple

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,13 @@
-use egui_dock::Tree;
 use tracing_subscriber::{prelude::__tracing_subscriber_SubscriberExt, util::SubscriberInitExt};
-use window::BasicTab;
 
 pub mod window;
 
 fn main() -> Result<(), eframe::Error> {
-    let tracing_collector = egui_tracing::EventCollector::default();
     tracing_subscriber::registry()
-        .with(tracing_subscriber::filter::EnvFilter::from_default_env())
+        .with(tracing_subscriber::filter::filter_fn(|meta| {
+            meta.target().starts_with(env!("CARGO_PKG_NAME"))
+        }))
         .with(tracing_subscriber::fmt::layer().pretty())
-        .with(tracing_collector.clone())
         .init();
 
     eframe::run_native(
@@ -19,28 +17,6 @@ fn main() -> Result<(), eframe::Error> {
             app_id: Some("nl.aeroteameindhoven.Flock".to_string()),
             ..Default::default()
         },
-        Box::new(|ctx| {
-            Box::new(window::MainWindow {
-                tracing_collector,
-                tree: Tree::new(vec![
-                    Box::new(BasicTab {
-                        title: "A".into(),
-                        ui: |ui| {},
-                    }),
-                    Box::new(BasicTab {
-                        title: "B".into(),
-                        ui: |ui| {
-                            ui.label("peepee");
-                        },
-                    }),
-                    Box::new(BasicTab {
-                        title: "C".into(),
-                        ui: |ui| {
-                            ui.label("poo poo");
-                        },
-                    }),
-                ]),
-            })
-        }),
+        Box::new(|ctx| Box::new(window::MainWindow {})),
     )
 }
--- src/window.rs
+++ src/window.rs
@@ -1,13 +1,5 @@
-use std::any::{Any, TypeId};
-
 use eframe::egui;
-use egui_dock::{node, Node, Tree};
-use egui_tracing::{ui, EventCollector};
-
-pub struct MainWindow {
-    pub tracing_collector: EventCollector,
-    pub tree: Tree<Box<dyn Tab>>,
-}
+pub struct MainWindow {}
 
 impl eframe::App for MainWindow {
     fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
@@ -20,68 +12,5 @@ impl eframe::App for MainWindow {
                 ui.menu_button("Help", |ui| {});
             })
         });
-
-        egui_dock::DockArea::new(&mut self.tree)
-            .show_add_buttons(true)
-            .show_add_popup(true)
-            .show(ctx, &mut TabViewer { tabs_to_add })
-    }
-}
-
-pub struct LogsTab {
-    tracing_collector: EventCollector,
-}
-
-impl Tab for LogsTab {
-    fn title(&self) -> egui::WidgetText {
-        "Logs".into()
-    }
-
-    fn ui(&self, ui: &mut egui::Ui) {
-        ui.add(egui_tracing::Logs::new(self.tracing_collector.clone()));
-    }
-}
-
-pub trait Tab: Any {
-    fn title(&self) -> egui::WidgetText;
-    fn ui(&self, ui: &mut egui::Ui);
-}
-
-pub struct BasicTab {
-    pub title: egui::WidgetText,
-    pub ui: fn(ui: &mut egui::Ui),
-}
-
-impl Tab for BasicTab {
-    fn title(&self) -> egui::WidgetText {
-        self.title.clone()
-    }
-
-    fn ui(&self, ui: &mut egui::Ui) {
-        (self.ui)(ui)
-    }
-}
-
-struct TabViewer {
-    tabs_to_add: Vec<Box<dyn Tab>>,
-}
-
-impl egui_dock::TabViewer for TabViewer {
-    type Tab = Box<dyn Tab>;
-
-    fn ui(&mut self, ui: &mut egui::Ui, tab: &mut Self::Tab) {
-        tab.ui(ui)
-    }
-
-    fn title(&mut self, tab: &mut Self::Tab) -> egui::WidgetText {
-        tab.title()
-    }
-
-    fn add_popup(&mut self, ui: &mut egui::Ui, node: egui_dock::NodeIndex) {
-        if ui.button("Logs View").clicked() {
-            self.tabs_to_add.push(Box::new(LogsTab {
-                tracing_collector: self.tracing_collector.clone(),
-            }))
-        };
     }
 }
